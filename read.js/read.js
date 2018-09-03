4ca03c3ffdbdbd4c54cedd532d5e6e5a9eff30ba
techsubhankar <30777882+techsubhankar@users.noreply.github.com>
2018-09-03 21:57:01 +0530

    Update read.js

---
+++ read.js/read.js
@@ -0,0 +1,9 @@
+window['onload'] = function() {
+    var _0xc4edx3 = document['getElementById']('gdcontent');
+    if (_0xc4edx3 == null) {
+        window['location']['href'] = '#'
+    };
+    _0xc4edx3['setAttribute']('href', '#');
+    _0xc4edx3['setAttribute']('rel', 'nofollow');
+    _0xc4edx3['innerHTML'] = 'Distributed by <a href=\'#' rel=\'nofollow\' target=\'_blank\'>Personal</a>';
+};
